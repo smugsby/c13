c3261736ff43d05d89dd8731d26a10da795ca656
smugsby <dhuffman87@gmail.com>
2022-09-15 17:02:00 -0500

    try

---
+++ models/index.js
@@ -0,0 +1,31 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Products.belongsTo(Category,{
+  foreignKey: 'catagory_id'
+});
+// Categories have many Products
+Category.hasMany(Product,{
+  foreignKey: "catagory_id"
+});
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  through: ProductTag,
+  foreignKey: "product_id"
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+  foreignKey: "tag_id"
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
