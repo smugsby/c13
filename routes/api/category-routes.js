8c8d74af963635edad37db0878f2f30a21695e13
smugsby <dhuffman87@gmail.com>
2022-09-22 21:33:06 -0500

    route work

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,27 +1,47 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, ProductTag } = require('../../models');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: ['id',
+      catagory_name
+    ]
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    attributes: [id,
+      ProductTag
+    ]
+
+  })
 });
 
 router.post('/', (req, res) => {
+  Category.create({
+
+  })
   // create a new category
 });
 
 router.put('/:id', (req, res) => {
+  Category.update({
+
+  })
   // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  Category.delete({
+
+  })
   // delete a category by its `id` value
 });
 
